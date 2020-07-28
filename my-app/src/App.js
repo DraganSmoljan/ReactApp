ea01057ea80614f42b65f631bbbe41e1170b8374
Dragan Smoljan <dragan.8.smoljan@gsk.com>
2020-07-29 00:20:30 +0200

    created an object from the response from api

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,19 +3,12 @@ import './App.css';
 import getWeather from "./adapters/openweathermap.adapter";
 
 function App() {
-  let obj;
+  
   useEffect(() => {
-    let obj = getWeather();
-    let weatherObject ={
-      weather: obj.weather,
-      main: obj.main,
-      wind: obj.wind
-    }
+   let weather = getWeather();
+   console.log(weather);
   }, []);
- 
 
-
-  console.log(weatherObject.main)
   return (
     <>
     hello world
--- my-app/src/adapters/openweathermap.adapter.js
+++ my-app/src/adapters/openweathermap.adapter.js
@@ -1,7 +1,15 @@
 import * as settings from '../config/openweathermap.json';
 import axios from "axios";
 
-export default function getWeather () {
-    console.log("ulaz");
-    return axios.get(`api.openweathermap.org/data/2.5/weather?q=${settings.city}&appid=${settings.key}`);
+export default async function getWeather () {
+   const weather = await axios.get(`${settings.host}?q=${settings.city}&appid=${settings.key}`)
+   .then(response => 
+    ({
+        "weather": response.data.weather,
+        "main" : response.data.main,
+        "wind" : response.data.wind
+    })
+    );
+   
+   return weather;
 }
